00c54c93da037dae06f8b2c81a0504dfd4b5567c
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-04-28 18:25:32 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,pro=1;     scanf("%d",&n);     while(n>0)     {         r=n%10;         n=n/10;         sum+=r;         pro*=r;     }     if(sum==pro)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     }
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
- int 
- main()
+int main()
 {
     int n,r,sum=0,pro=1;
     scanf("%d",&n);
     while(n>0)
     {
         r=n%10;
+        n=n/10;
         sum+=r;
         pro*=r;
-        n=n/10;
     }
     if(sum==pro)
     {
@@ -19,5 +18,5 @@
     {
         printf("Not Spy Number");
     }
-    return 0;
+    
 }
